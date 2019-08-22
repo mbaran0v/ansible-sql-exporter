cadc057027e0ee727d0b4f664672fb0024e58ea5
Maxim Baranov <maksim.baranov@gmail.com>
2019-08-22 16:37:16 +0400

    fix tests (#3)

--- molecule/resources/tests/all/test_common.py
+++ molecule/resources/tests/all/test_common.py
@@ -1,27 +1,53 @@
 
 debian_os = ['debian', 'ubuntu']
 rhel_os = ['redhat', 'centos']
+version = '0.2.0'
+root_dir = '/opt/sql_exporter'
 
 
 def test_distribution(host):
     assert host.system_info.distribution.lower() in debian_os + rhel_os
 
 
-def test_config(host):
-    f = host.file('/opt/sql_exporter/current/sql_exporter.yml')
+def test_install_dir(host):
+    f = host.file(root_dir)
 
     assert f.exists
+    assert f.is_directory
 
 
-def test_socket(host):
-    s = host.socket('tcp://:::9237')
-    print(host.socket.get_listening_sockets())
+def test_release_dir(host):
+    f = host.file(root_dir + '/releases/' + version)
 
-    assert s.is_listening
+    assert f.exists
+    assert f.is_directory
 
 
-def test_serice(host):
+def test_release_symlink_dir(host):
+    f = host.file(root_dir + '/current')
+
+    assert f.exists
+    assert f.is_symlink
+    assert f.linked_to == root_dir + '/releases/' + version
+
+
+def test_service(host):
     s = host.service('sql_exporter')
 
     assert s.is_enabled
     assert s.is_running
+
+
+def test_user(host):
+    u = host.user('sql-exp')
+
+    assert u.exists
+    assert u.group == 'sql-exp'
+    assert u.shell == '/usr/sbin/nologin'
+
+
+def test_config(host):
+    f = host.file(root_dir + '/releases/' + version + '/sql_exporter.yml')
+
+    assert f.exists
+    assert oct(f.mode) == '0o600'
